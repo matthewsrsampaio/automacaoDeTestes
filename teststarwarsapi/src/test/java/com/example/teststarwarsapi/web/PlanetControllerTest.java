3a3b49a201b6cce2d70b9ddea4c253c127c08018
MatthewsRSampaio <matthews_venom@hotmail.com>
2025-03-06 17:05:43 -0300

    descarte

--- teststarwarsapi/src/test/java/com/example/teststarwarsapi/web/PlanetControllerTest.java
+++ teststarwarsapi/src/test/java/com/example/teststarwarsapi/web/PlanetControllerTest.java
@@ -1,10 +1,12 @@
 package com.example.teststarwarsapi.web;
 
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -37,6 +39,17 @@ public class PlanetControllerTest {
                .content(objectMapper.writeValueAsString(PLANET)).contentType(MediaType.APPLICATION_JSON))
                .andExpect(status().isCreated())
                .andExpect(jsonPath("$").value(PLANET)); //Esse cifrão referencia á raiz do JSON (primeira propriedade)
+    }
+
+    @Test
+    public void createPlanet_WithExistingName_ReturnsConflict() throws Exception {
+        //STUB - expectativa de ação e resposta
+        when(planetService.create(any())).thenThrow(DataIntegrityViolationException.class);
+
+        mockMvc.perform(post("/planets")
+               .content(objectMapper.writeValueAsString(PLANET))
+               .contentType(MediaType.APPLICATION_JSON))
+               .andExpect(status().isConflict());
     }
     
 }
