e7059037372962926a62222cb8e742419a2f3d9d
MatthewsRSampaio <matthews_venom@hotmail.com>
2025-03-12 16:04:33 -0300

    testes subcutaneos

--- teststarwarsapi/src/test/java/com/example/teststarwarsapi/PlanetIT.java
+++ teststarwarsapi/src/test/java/com/example/teststarwarsapi/PlanetIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -13,9 +14,14 @@ import org.springframework.test.context.jdbc.Sql;
 import com.example.teststarwarsapi.domain.Planet;
 
 import static common.PlanetConstants.PLANET;
+import static common.PlanetConstants.PLANETS;
+import static common.PlanetConstants.TATOOINE;
+
+//T E S T E S     S U B C U T Â N E O S
 
 @ActiveProfiles("it")
 @SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = { "/import_planets.sql" }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD) //Cria o banco antes dos testes    
 @Sql(scripts = { "/remove_planets.sql" }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD) //Limpa o banco depois dos testes    
 public class PlanetIT {
     @Autowired
@@ -38,6 +44,58 @@ public class PlanetIT {
 
     }
 
+    @Test
+    public void getPlanet_ReturnsPlanet() {
+        ResponseEntity<Planet> sut = restTemplate.getForEntity("/planets/1", Planet.class);
 
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void getPlanetByName_ReturnsPlanet() {
+        ResponseEntity<Planet> sut = restTemplate
+            .getForEntity("/planets/name/" + TATOOINE.getName(), Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void listPlanets_ReturnsAllPlanets() {
+        ResponseEntity<Planet[]> sut = restTemplate.getForEntity("/planets", Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()).isNotEmpty();
+        assertThat(sut.getBody()).isNotNull();
+        assertThat(sut.getBody().length).isEqualTo(PLANETS.size());
+        assertThat(sut.getBody()).hasSize(PLANETS.size());
+    }
+
+    @Test
+    public void listPlanets__ByClimate_ReturnsPlanets() {
+        ResponseEntity<Planet[]> sut = restTemplate
+            .getForEntity("/planets?climate=" + TATOOINE.getClimate(), Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()[0]).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void listPlanets_ByTerrain_ReturnsPlanets() {
+        ResponseEntity<Planet[]> sut = restTemplate
+            .getForEntity("/planets?terrain=" + TATOOINE.getTerrain(), Planet[].class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(sut.getBody()[0]).isEqualTo(TATOOINE);
+    }
+
+    @Test
+    public void removePlanet_ReturnsNoContent() {
+        ResponseEntity<Void> sut = restTemplate
+            .exchange("/planets/" + TATOOINE.getId(), HttpMethod.DELETE, null, void.class);
+
+            assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
 
 }
